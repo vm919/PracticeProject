0bc317a58f56dc33271fbcb6101786db2f2e515a
Vaibhav <vaibhav91nine@gmail.com>
2024-01-18 18:35:24 -0800

    added a new method for adding numbers

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -10,11 +10,18 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Jai Shree Ram"); 
+		add(3,6);
 	}
 	
 	//static block
 	static{
 		System.out.println("Entered into Static block");
+	}
+	
+	//adding a new method for pull request
+	public static void add(int a, int b) {
+		int c = a+b;
+		System.out.println("The value of c is: " + c);
 	}
 
 	
