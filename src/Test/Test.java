dc265860006c569142a89d6d9685508a2160dee6
Vaibhav <vaibhav91nine@gmail.com>
2024-01-18 18:04:47 -0800

    change made in Static block

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -9,11 +9,13 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Jai Shree Ram");
+		System.out.println("Jai Shree Ram"); 
 	}
 	
 	//static block
-	static{}
+	static{
+		System.out.println("Entered into Static block");
+	}
 
 	
 	
